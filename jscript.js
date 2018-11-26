69311394e86a3e969ef21c475ad39da9a1284c88
fullstackJST <mynextgiz@gmail.com>
2018-11-26 20:15:27 +0530

    Intial Commit index.html, style.css, jscript.js

---
+++ jscript.js
@@ -0,0 +1,23 @@
+// var fullName = "Shubham Kumar"
+
+// var age = 25;
+// var sentence = "Hello, my name is " + fullName + ".\nI'll be " + (age + 1) + " years old next year.";
+// console.log(sentence);
+
+
+// ToString
+
+// let value = true;
+// console.log(typeof value);
+
+
+// value = Number(value);
+// console.log(typeof value);
+
+
+let i = 0;
+    do{
+        console.log(i);
+        i++;
+    }
+    while(i<1);
